0cbf4285524903f51d140eb402f45b25eb4e57bd
Kimchy101 <kimchy101@gmail.com>
2018-04-11 20:55:07 -0500

    major css revamp as well as better mobile support and about page

---
+++ info-particle.js
@@ -0,0 +1,62 @@
+particlesJS("particles-js", {
+  "particles": {
+    "number": {
+      "value": 30,
+      "density": {
+        "enable": true,
+        "value_area": 700
+      }
+    },
+    "color": {
+      "value": ["#ffb3b3", "#bfb7df", "#e48ff9", "#9fde81"]
+    },
+    "shape": {
+      "type": "polygon",
+      "stroke": {
+        "width": 0,
+        "color": "#000000"
+      },
+      "polygon": {
+        "nb_sides": 4
+      },
+    },
+    "size": {
+      "value": 35,
+      "random": true,
+    },
+    "line_linked": {
+      "enable": false,
+    },
+    "move": {
+      "enable": true,
+      "speed": 1,
+      "direction": "top",
+      "random": false,
+      "straight": false,
+      "out_mode": "out",
+      "bounce": false,
+      "attract": {
+        "enable": false,
+        "rotateX": 600,
+        "rotateY": 1200
+      }
+    }
+  },
+  
+"interactivity": {
+    "detect_on": "canvas",
+    "events": {
+      "onhover": {
+        "enable": false,
+      },
+      "onclick": {
+        "enable": false,
+      },
+      "resize": true
+    },
+  },
+
+  "retina_detect": true
+});
+
+  
